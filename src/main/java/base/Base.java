656528b47e6ebfee0ff9919f1f3a95f8fc529a77
mostafa <mostafametwaly86@gmail.com>
2022-04-12 15:43:29 +0200

    update commit

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -14,9 +14,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
-
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
